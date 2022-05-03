d6bba90117c1e64d01e2e634d92c26129ae62b74
Matthew Won <matthewwon.gg.no.re@gmail.com>
2022-05-02 23:22:12 -0400

    reversing testing

--- components/templates/layouts/page.tsx
+++ components/templates/layouts/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Page({ children }) {
-    return <div className="min-h-screen w-full px-5 py-8">{children}</div>
+    return <div className="min-h-screen w-full px-4 py-8">{children}</div>
 }
