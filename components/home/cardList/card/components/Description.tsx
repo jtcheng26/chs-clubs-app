7384d39c6ba2ce68a8b95a5cd9c302fd15eaf873
Matthew Won <matthewwon.gg.no.re@gmail.com>
2022-05-09 13:39:54 -0400

    Description.tsx: changed font color

--- components/home/cardList/card/components/Description.tsx
+++ components/home/cardList/card/components/Description.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Description() {
-    return <div className="text-sm text-[#ababab] font-light">
+    return <div className="text-sm text-[#828282] font-light">
         Description
     </div>
     
