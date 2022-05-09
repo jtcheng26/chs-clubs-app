0dab7b9d33e6565faa54b441ece6078eff8b157b
jtcheng26 <jtcheng226@gmail.com>
2022-05-09 13:51:40 -0400

    Add onClick prop to category component

--- components/home/cardList/card/components/Category.tsx
+++ components/home/cardList/card/components/Category.tsx
@@ -4,9 +4,15 @@ interface CategoryProps {
     name: string
     color: string
     selected: boolean
+    onClick: () => void
 }
 
-export default function Category({ name, color, selected }: CategoryProps) {
+export default function Category({
+    name,
+    color,
+    onClick,
+    selected,
+}: CategoryProps) {
     const defaultColor = 'bg-gray-500'
     return <div>Category</div>
 }
