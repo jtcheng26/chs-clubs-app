c490578360bc307f135eeb1cfac183c0f5c6805c
jtcheng26 <jtcheng226@gmail.com>
2022-05-11 07:42:17 -0400

    Important link component template

---
+++ components/home/cardList/card/components/ImportantLink.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface ImportantLinkProps {
+    href: string
+    children: React.ReactNode
+}
+
+export default function ImportantLink({ href, children }: ImportantLinkProps) {
+    return <div>ImportantLink</div>
+}
