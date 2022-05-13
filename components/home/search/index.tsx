b8a93c6dffb76ff5f185460534bbc279a9eea9e9
Matthew Won <matthewwon.gg.no.re@gmail.com>
2022-05-13 12:56:38 -0400

    created search bar

--- components/home/search/index.tsx
+++ components/home/search/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Org } from '@prisma/client'
 
-export default function SearchBar({ setSearchQuery }) {
-    return <div>SearchBar</div>
+
+export default function Filter({ placeholder, setSearchTags }) {
+    return (
+    <div className="search">
+        <div className="searchInputs">
+            <input type="text" placeholder=" Search"/>
+            <div className="searchIcon"></div>
+        </div>
+        <div className="dataResults"></div>
+    
+    </div>
+    )
 }
