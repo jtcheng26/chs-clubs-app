9d25b6658ab93670526ba12acb2224c0462dd64b
jtcheng26 <jtcheng226@gmail.com>
2022-05-20 00:21:26 -0400

    Include links on club pages

--- pages/club/[name].tsx
+++ pages/club/[name].tsx
@@ -43,6 +43,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
         include: {
             sponsors: true,
             categories: true,
+            links: true,
         },
     })
     return {
